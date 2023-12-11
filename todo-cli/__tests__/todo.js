9946f75e70b31439661095462f1fb4002485da91
SambaruSrinath <115223428+SambaruSrinath@users.noreply.github.com>
2023-12-11 19:03:27 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,3 +1,79 @@
+const todos = [];
+
+// Function to add new todo item
+function addTodo(description) {
+    todos.push({
+        id: Date.now(), // Unique identifier
+        description,
+        completed: false,
+        dueDate: null, // Optional due date
+    });
+}
+
+// Function to mark a todo as completed
+function markAsCompleted(id) {
+    const todo = todos.find((todo) => todo.id === id);
+    if (todo) {
+        todo.completed = true;
+    }
+}
+
+// Function to retrieve overdue items
+function getOverdueItems() {
+    const today = new Date();
+    return todos.filter((todo) => {
+        if (todo.dueDate && todo.dueDate < today && !todo.completed) {
+            return true;
+        }
+        return false;
+    });
+}
+
+// Function to retrieve due today items
+function getDueTodayItems() {
+    const today = new Date();
+    return todos.filter((todo) => {
+        if (
+            todo.dueDate &&
+            today.toDateString() === new Date(todo.dueDate).toDateString() &&
+            !todo.completed
+        ) {
+            return true;
+        }
+        return false;
+    });
+}
+
+// Function to retrieve due later items
+function getDueLaterItems() {
+    const today = new Date();
+    return todos.filter((todo) => {
+        if (todo.dueDate && todo.dueDate > today && !todo.completed) {
+            return true;
+        }
+        return false;
+    });
+}
+
+// Function to convert internal data to a displayable list
+function toDisplayableList() {
+    let output = "";
+    for (const todo of todos) {
+        const status = todo.completed ? "[X]" : "[ ]";
+        output += `${status} ${todo.description}\n`;
+    }
+    return output;
+}
+
+// Example usage
+addTodo("Buy groceries");
+addTodo("Finish homework", new Date("2023-12-12"));
+addTodo("Clean the room", new Date("2023-12-14"));
+
+markAsCompleted(todos[0].id);
+
+console.log(toDisplayableList());
+
 /* eslint-disable no-undef */
 it("should add a new todo item", () => {
     const initialLength = todos.length;
