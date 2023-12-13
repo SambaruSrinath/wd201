42d54ca904c754eb167f17ca06a444f1ae513f7d
SambaruSrinath <115223428+SambaruSrinath@users.noreply.github.com>
2023-12-13 14:46:47 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,17 +1,20 @@
+// todo.js
+
 /* eslint-disable no-undef */
 const todoList = () => {
     const all = [];
+
     const add = (todoItem) => {
         all.push(todoItem);
     };
+
     const markAsComplete = (index) => {
         all[index].completed = true;
     };
 
     const overdue = () => {
-        return all.filter(
-            (item) => item.dueDate < new Date().toLocaleDateString("en-CA")
-        );
+        const today = new Date().toLocaleDateString("en-CA");
+        return all.filter((item) => item.dueDate < today);
     };
 
     const dueToday = () => {
@@ -20,45 +23,15 @@ const todoList = () => {
     };
 
     const dueLater = () => {
-        return all.filter(
-            (item) => item.dueDate > new Date().toLocaleDateString("en-CA")
-        );
+        const today = new Date().toLocaleDateString("en-CA");
+        return all.filter((item) => item.dueDate > today);
     };
 
     const toDisplayableList = (list) => {
-        const dsl = [];
-        list.forEach((element) => {
-            if (element.dueDate === today) {
-                if (element.completed === true) {
-                    const a = "[x] " + element.title;
-                    dsl.push(a);
-                } else {
-                    const a = "[ ] " + element.title;
-                    dsl.push(a);
-                }
-            } else {
-                if (element.completed === true) {
-                    const a = "[x] " + element.title + " " + element.dueDate;
-                    dsl.push(a);
-                } else {
-                    const a = "[ ] " + element.title + " " + element.dueDate;
-                    dsl.push(a);
-                }
-            }
-        });
-        let g = "";
-        for (let i = 0; i < dsl.length; i++) {
-            // eslint-disable-next-line no-undef
-            obj = dsl[i];
-            if (i === 0) {
-                // eslint-disable-next-line no-undef
-                g = g + obj;
-            } else {
-                // eslint-disable-next-line no-undef
-                g = g + "\n" + obj;
-            }
-        }
-        return g;
+        return list.map((element) => {
+            const status = element.completed ? "[x]" : "[ ]";
+            return `${status} ${element.title} ${element.dueDate}`;
+        }).join("\n");
     };
 
     return {
@@ -73,3 +46,4 @@ const todoList = () => {
 };
 
 module.exports = todoList;
+
