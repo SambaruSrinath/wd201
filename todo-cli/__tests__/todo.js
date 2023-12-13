094f3bf842504f96cab091a107e0283f64b898c1
SambaruSrinath <115223428+SambaruSrinath@users.noreply.github.com>
2023-12-13 17:00:59 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,24 +1,36 @@
-/* eslint-disable no-unused-vars */
-// tests/todo.js
+let todoList = require("../todo");
 
-const todoList = require("../todo");
-
-const { all, markAsComplete, add, overdue, dueToday, dueLater, toDisplayableList } = todoList();
-
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 /* eslint-disable no-undef */
-describe("TodoList Test Suite", () => {
+describe("TodoList  Test Suite", () => {
     beforeAll(() => {
+        // Seed the test data
         [
-            { title: "pay rent", completed: false, dueDate: new Date().toLocaleDateString("en-CA") },
-            { title: "buy milk", completed: false, dueDate: new Date().toLocaleDateString("en-CA") },
-            { title: "do homework", completed: false, dueDate: new Date().toLocaleDateString("en-CA") },
+            {
+                title: "pay rent",
+                completed: false,
+                dueDate: new Date().toLocaleDateString("en-CA"),
+            },
+            {
+                title: "buy milk",
+                completed: false,
+                dueDate: new Date().toLocaleDateString("en-CA"),
+            },
+            {
+                title: "do homework",
+                completed: false,
+                dueDate: new Date().toLocaleDateString("en-CA"),
+            },
         ].forEach(add);
     });
-
     test("Should add a new todo", () => {
         expect(all.length).toEqual(3);
 
-        add({ title: "A test item", completed: false, dueDate: new Date().toLocaleDateString("en-CA") });
+        add({
+            title: "A test item",
+            completed: false,
+            dueDate: new Date().toLocaleDateString("en-CA"),
+        });
 
         expect(all.length).toEqual(4);
     });
@@ -34,12 +46,10 @@ describe("TodoList Test Suite", () => {
     });
 
     test("Should retrieve due today items", () => {
-        expect(dueToday().length).toEqual(4); // Assuming the seed data has an item due today
+        expect(dueToday().length).toEqual(4);
     });
 
     test("Should retrieve due later items", () => {
         expect(dueLater().length).toEqual(0);
     });
-
-
 });
